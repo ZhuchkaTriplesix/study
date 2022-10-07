55e57f64adf382baea2f42f43e53a19bd4944fd7
JuntexZhuk <mrlololoshka94@gmail.com>
2022-10-08 02:09:15 +0300

    feat(function): main menu, calc function

--- main.py
+++ main.py
@@ -1,4 +1,46 @@
-print("Input ur bot name")
-x = str(input())
-print("Your bot name", x)
-print("Hello, World!")
+import math
+
+
+def convert():
+    Start = input("Choose one: (calc, factorial, sqrteq):")
+    if Start not in ("calc", "factorial", "sqrteq"):
+        print("invalid argument")
+    else:
+        if Start == "calc":
+            what = input("Choose operator (+, -, *, /, //, %): ")
+
+            if what not in ("+", "-", "*", "%", "/", "//"):
+                print("invalid character")
+            else:
+                a = float(input("First number: "))
+                b = float(input("Second number: "))
+                if what == "+":
+                    c = a + b
+                    print("result:" + str(c))
+                elif what == "-":
+                    c = a - b
+                    print("result:" + str(c))
+                elif what == "*":
+                    c = a * b
+                    print("result:" + str(c))
+                elif what == "/":
+                    c = a / b
+                    print("result:" + str(c))
+                elif what == "//":
+                    c = a // b
+                    print("result:" + str(c))
+                elif what == "%":
+                    c = a % b
+                    print("result:" + str(c))
+        #elif Start == "sqrteq":
+
+
+
+convert()
+while True:
+    flag = input("One more time? [y/n]: ")
+
+    if flag == 'y':
+        convert()
+    else:
+        break
