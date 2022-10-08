4a14fc93a137ab4556ee5ccb4283708c740e0358
JuntexZhuk <mrlololoshka94@gmail.com>
2022-10-08 15:55:17 +0300

    Fix(Input):Fixed bug with input in calc and factorial

--- main.py
+++ main.py
@@ -1,51 +1,72 @@
 import math
 
 
-# add a new function factorial
+# add a new function sqrteq
+
+def floatCheck(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
 
 
 def convert():
-    Start = input("Choose one: (calc, factorial):")
-    if Start not in ("calc", "factorial", "sqrteq"):
-        print("invalid argument")
+    start = input("Choose one: \n1. Calc\n2. Factorial \n")
+    if start not in ("1", "2", "3"):
+        print("Invalid argument")
     else:
-        if Start == "calc":
-            what = input("Choose operator (+, -, *, /, //, %): ")
-
+        if start == "1":
+            what = input("Choose the operator (+, -, *, /, //, %): ")
             if what not in ("+", "-", "*", "%", "/", "//"):
-                print("invalid character")
+                print("Invalid character")
             else:
-                a = float(input("First number: "))
-                b = float(input("Second number: "))
-                if what == "+":
-                    c = a + b
-                    print("result: " + str(c))
-                elif what == "-":
-                    c = a - b
-                    print("result: " + str(c))
-                elif what == "*":
-                    c = a * b
-                    print("result: " + str(c))
-                elif what == "/":
-                    c = a / b
-                    print("result: " + str(c))
-                elif what == "//":
-                    c = a // b
-                    print("result: " + str(c))
-                elif what == "%":
-                    c = a % b
-                    print("result: " + str(c))
-        elif Start == "factorial":
+                a = input("First number: ")
+                b = input("Second number: ")
+                bool = floatCheck(a)
+                bool1 = a.isdigit()
+                if bool or bool1:
+                    bool = floatCheck(b)
+                    bool1 = b.isdigit()
+                    if bool or bool1:
+                        if what == "+":
+                            res = float(a) + float(b)
+                            print("Result: " + str(res))
+                        elif what == "-":
+                            res = float(a) - float(b)
+                            print("Result: " + str(res))
+                        elif what == "*":
+                            res = float(a) * float(b)
+                            print("Result: " + str(res))
+                        elif what == "/":
+                            res = float(a) / float(b)
+                            print("Result: " + str(res))
+                        elif what == "//":
+                            res = float(a) // float(b)
+                            print("Result: " + str(res))
+                        elif what == "%":
+                            res = float(a) % float(b)
+                            print("Result: " + str(res))
+                    else:
+                        print("Invalid second input")
+                else:
+                    print("Invalid first input")
+
+        elif start == "2":
             method = input("Choose the method: (loop)")
-            if method not in ("loop", "recursion"):
-                print("invalid character")
+            if method not in ("\n1. Loop \n2. Recursion"):
+                print("Invalid character")
             else:
-                if method == "loop":
-                    number = int(input("Input your number: "))
-                    factorial = 1
-                    for i in range(2, number + 1):
-                        factorial *= i
-                    print(factorial)
+                if method == "1":
+                    number = input("Input your number: ")
+                    bool = number.isdigit()
+                    if bool:
+                        factorial = 1
+                        for i in range(2, int(number) + 1):
+                            factorial *= i
+                        print(factorial)
+                    else:
+                        print("Invalid input")
 
 
 convert()
